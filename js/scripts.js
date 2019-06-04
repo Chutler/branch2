6429f03ec068c9c017d034ed5cd331d03d850925
Craig Hutler and Diego Rojas <diego@>
2019-06-03 20:48:55 -0700

    adds additional HTML

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,17 @@
 $(document).ready(function() {
-  $("form#insurance").submit(function(event) {
+  $("form#user").submit(function(event) {
     var age = parseInt($("input#age").val());
     var gender = $("select#gender").val();
+    var weight = parseInt($("input#weight").val());
+    var hairColor = $("input#hairColor")
 
     var quote = (100 - age) * 3;
     if (gender === 'male' && age < 26) {
       quote += 50;
     }
 
-    $("#rate").text(quote);
-    $("#quote").show()
+    $("#date").text(quote);
+    $("#yourDate").show()
 
     event.preventDefault();
   });
